b3fde6a8ecae182a9a47debcdef67e6de0eed412
studywell2 <studywellmail1@gmail.com>
2026-06-28 19:52:02 -0700

    message

--- resources/views/layouts/school.blade.php
+++ resources/views/layouts/school.blade.php
@@ -1314,6 +1314,13 @@
             <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
             More
         </button>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="mobile-bottom-nav-item">
+                <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"/></svg>
+                Sign Out
+            </button>
+        </form>
     </nav>
     <style>
         @media(max-width: 767px) {
